cf042f68baec53ee4b4a35efb45317a8071bbe43
Ayush Sonar <ayusht1d@gmail.com>
2025-01-19 17:49:00 +0530

    refactor: reorganize ParticipantList component and update import paths

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import ParticipantList from "../components/ParticipantList";
+import ParticipantList from "./ParticipantList";
 
 function Home() {
   return (
     <>
-      HELLO WORLD
       <ParticipantList />
     </>
   );
